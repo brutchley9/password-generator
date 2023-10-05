2b19faae561522aa73b6e19cca6c2fee4409763c
brutchley <bhutchison0901@yahoo.com>
2023-10-04 20:58:13 -0500

    adding more variables including outlines for if/else functions

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ Follow link https://github.com/brutchley9/module-3-challenge to access GitHub re
 <li>https://www.w3schools.com/jsref/met_win_prompt.asp</li>
 
 <li>https://www.w3schools.com/js/js_random.asp</li>
+
+<li>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random</li>
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
 // Assignment code here
-var uppercaseLetters = prompt("Welcome to my password generator! Would you like to conlude Uppercase letters?")
-var lowercaseletters = prompt("Would you like to include lower case letters?")
-var numbers = prompt("Would you like to include numbers?")
-var specialCharacters = prompt("Would you like to include special characters?")
+var uppercaseLetters = prompt("Welcome to my password generator! Would you like to conlude Uppercase letters? (yes/no)")
+var lowercaseLetters = prompt("Would you like to include lower case letters? (yes/no)")
+var numbers = prompt("Would you like to include numbers? (yes/no)")
+var specialCharacters = prompt("Would you like to include special characters? (yes/no)")
 var passwordLength = prompt("How long should password be (between 8 and 128 characters)?")
+
+var specialCharactertypes = ["!", "?", "@", "#", "$", "%", "^", "&", "*", ";"]
+var uppercaseLetterstypes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z"]
+var lowercaseLetterstypes = ["a", "b", "d", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+Math.floor(Math.random() * 999)
+
+
+
+if (uppercaseLetters === "yes" && uppercaseLetters === "Yes" && uppercaseLetters === "YES") {
+  
+}
+
+
+if (lowercaseLetters === "yes" && lowercaseLetters === "Yes" && lowercaseLetters === "YES") {
+  
+}
+
+
+if (numbers === "yes" && numbers === "Yes" && numbers === "YES") {
+  Math.floor(Math.random() * 999)
+  
+}
+
+
+if (specialCharacters === "yes" && specialCharacters === "Yes" && specialCharacters === "YES") {
+  
+}
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
