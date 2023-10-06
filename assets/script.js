e3977e9c063504c9f4fe7a92805986813a1f7c47
brutchley <bhutchison0901@yahoo.com>
2023-10-05 22:05:45 -0500

    adding functions to produce random assortment of values within arrays

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,59 @@
 // Assignment code here
-var uppercaseLetters = prompt("Welcome to my password generator! Would you like to conlude Uppercase letters? (yes/no)")
-var lowercaseLetters = prompt("Would you like to include lower case letters? (yes/no)")
-var numbers = prompt("Would you like to include numbers? (yes/no)")
-var specialCharacters = prompt("Would you like to include special characters? (yes/no)")
+var uppercaseLetters = confirm("Welcome to my password generator! Would you like to conlude Uppercase letters? (yes/no)")
+var lowercaseLetters = confirm("Would you like to include lower case letters? (yes/no)")
+var numbers = confirm("Would you like to include numbers? (yes/no)")
+var specialCharacters = confirm("Would you like to include special characters? (yes/no)")
 var passwordLength = prompt("How long should password be (between 8 and 128 characters)?")
+
+// these are my assortment of variables for the password generator, with paramters included below
 
 var specialCharactertypes = ["!", "?", "@", "#", "$", "%", "^", "&", "*", ";"]
 var uppercaseLetterstypes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z"]
 var lowercaseLetterstypes = ["a", "b", "d", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-Math.floor(Math.random() * 999)
 
+//series of functions designed to produce password generator
 
+function gitrandomindex(arraylength) {
+  return Math.floor(Math.random() * arraylength)
+}
 
-if (uppercaseLetters === "yes" && uppercaseLetters === "Yes" && uppercaseLetters === "YES") {
-  
+var randomIndex = gitrandomindex(specialCharactertypes.length)
+
+if (uppercaseLetters) {
+
+} else {
+  console.log("")
 }
 
 
 if (lowercaseLetters === "yes" && lowercaseLetters === "Yes" && lowercaseLetters === "YES") {
   
+} else {
+  console.log("")
 }
 
 
 if (numbers === "yes" && numbers === "Yes" && numbers === "YES") {
-  Math.floor(Math.random() * 999)
+  console.log(Math.floor(Math.random() * 999))
   
+} else {
+  console.log("")
 }
 
 
 if (specialCharacters === "yes" && specialCharacters === "Yes" && specialCharacters === "YES") {
   
+} else {
+  console.log("")
 }
 
 
 
 
-
+function generatePassword() {
+  console.log(numbers)
+}
 
 
 
